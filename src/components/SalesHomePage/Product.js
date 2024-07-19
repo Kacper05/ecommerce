ee05916a2bb00db035aeef27229ea9ef5e670621
Kacper <kacperkraszkiewicz7@gmail.com>
2024-07-19 15:35:19 +0200

    t

--- src/components/SalesHomePage/Product.js
+++ src/components/SalesHomePage/Product.js
@@ -1,8 +1,20 @@
+import { useState } from "react"
+
 function Product(props){
+    const [display,setDisplay] = useState('none')
+    const ShowButton = ()=>{
+        setDisplay('contents')
+    }
+    const HideButton =()=>{
+        setDisplay('none')
+    }
     return(
-        <div className="product">
+        <div className="product" onMouseOver={ShowButton} onMouseOut={HideButton}>
             <div className="product-image-box">
                 <img src={props.image} alt=""/>
+                <div className="product-image-box-button" style={{display:display, width:'100%'}}>
+                    <button style={{width:'100%'}}>Add To Cart</button>
+                </div>
             </div>
             <div className="product-details">
                 <div className="product-name">{props.name}</div>
